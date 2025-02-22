daf82cd4a5a918fac78d8aaedeb0bb5314acc52c
ANkulagin <ankulskull@gmail.com>
2025-02-22 14:21:01 +0300

    SB-1 | Added file_repository

---
+++ internal/repository/file_repository.go
@@ -0,0 +1,42 @@
+package repository
+
+import "os"
+
+type FileRepository interface {
+	ReadFile(path string) (string, error)
+	WriteFile(path, content string) error
+	AppendToFile(path, content string) error
+}
+
+type fileRepository struct{}
+
+func NewFileRepository() FileRepository {
+	return &fileRepository{}
+}
+
+func (fr *fileRepository) ReadFile(path string) (string, error) {
+	data, err := os.ReadFile(path)
+	if err != nil {
+		return "", err
+	}
+	return string(data), nil
+}
+
+func (fr *fileRepository) WriteFile(path, content string) error {
+	return os.WriteFile(path, []byte(content), os.ModePerm)
+}
+func (fr *fileRepository) AppendToFile(path, content string) error {
+	f, err := os.OpenFile(path, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		return err
+	}
+	defer func(f *os.File) {
+		_ = f.Close()
+	}(f)
+
+	if _, err := f.WriteString(content + "\n"); err != nil {
+		return err
+	}
+
+	return nil
+}
