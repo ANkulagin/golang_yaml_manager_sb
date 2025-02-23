fc0a9ef00870501249eaab69ce31118619cf0ba8
ANkulagin <ankulskull@gmail.com>
2025-02-23 19:25:51 +0300

    SB-1 | Added LoadFrontMatter + test

--- internal/domain/note.go
+++ internal/domain/note.go
@@ -1,6 +1,10 @@
 package domain
 
-import "strings"
+import (
+	"errors"
+	"gopkg.in/yaml.v2"
+	"strings"
+)
 
 type Note struct {
 	FilePath    string
@@ -9,5 +13,20 @@ type Note struct {
 }
 
 func (n *Note) HasYaml() bool {
-	return strings.HasPrefix(n.Content, "---")
+	return strings.HasPrefix(strings.TrimLeft(n.Content, "\t\n\r"), "---")
+}
+
+// todo продумать режим где будет просто пропускать сломанный yaml
+func (n *Note) LoadFrontMatter() error {
+	if !n.HasYaml() {
+		return errors.New("front matter not found")
+	}
+	parts := strings.SplitN(n.Content, "---", 3)
+	if len(parts) < 3 {
+		return errors.New("incorrectly format front matter")
+	}
+	if err := yaml.Unmarshal([]byte(parts[1]), &n.FrontMatter); err != nil {
+		return err
+	}
+	return nil
 }
--- internal/domain/note_test.go
+++ internal/domain/note_test.go
@@ -36,7 +36,7 @@ func TestNote_HasYaml(t *testing.T) {
 		{
 			name:     "empty line at the beginning of the file",
 			content:  "\n\n---\nHello, world!\n---",
-			expected: false,
+			expected: true,
 		},
 	}
 
@@ -49,6 +49,102 @@ func TestNote_HasYaml(t *testing.T) {
 			result := sut.HasYaml()
 
 			require.Equal(t, tc.expected, result)
+		})
+	}
+}
+
+func TestNote_LoadFrontMatter_Success(t *testing.T) {
+	sut := &Note{}
+
+	testCases := []struct {
+		name     string
+		content  string
+		expected map[string]any
+	}{
+		{
+			name: "success",
+			content: `
+---
+title:  wating
+source1: 
+author: ANkulagin
+closed: false
+---
+`,
+			expected: map[string]any{
+				"title":   "wating",
+				"source1": nil,
+				"author":  "ANkulagin",
+				"closed":  false,
+			},
+		},
+	}
+
+	for _, tc := range testCases {
+		tc := tc
+		t.Run(tc.name, func(t *testing.T) {
+			t.Parallel()
+			sut.Content = tc.content
+
+			err := sut.LoadFrontMatter()
+			require.NoError(t, err)
+			require.Equal(t, tc.expected, sut.FrontMatter)
+		})
+	}
+}
+
+func TestNote_LoadFrontMatter_Error(t *testing.T) {
+	sut := &Note{}
+
+	testCases := []struct {
+		name           string
+		content        string
+		expectedErrMsg string
+	}{
+		{
+			name:           "empty content",
+			content:        "",
+			expectedErrMsg: "front matter not found",
+		},
+		{
+			name:           "--- off the top  of front matter",
+			content:        "off the top ---  of front matter",
+			expectedErrMsg: "front matter not found",
+		},
+		{
+			name:           "forgot to close",
+			content:        "\n---\nclose:false\n forgot to close",
+			expectedErrMsg: "incorrectly format front matter",
+		},
+		{
+			name: "forgot to close",
+			content: `
+---
+title:  wating
+source1: 
+source2: 
+source3: 
+source4: 
+author: ANkulagin
+date: 2025-01-05
+description: 
+tags: 
+closed:false
+---
+`, //closed: false ✅ closed:false ❌
+			expectedErrMsg: "",
+		},
+	}
+
+	for _, tc := range testCases {
+		tc := tc
+		t.Run(tc.name, func(t *testing.T) {
+			t.Parallel()
+			sut.Content = tc.content
+
+			err := sut.LoadFrontMatter()
+			require.Error(t, err)
+			require.Contains(t, err.Error(), tc.expectedErrMsg)
 		})
 	}
 }
